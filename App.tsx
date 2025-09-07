7921ad5a3fc44c49ae2dee340b3d482230a63a24
Jaczibeth Cruz <jaczicruz@gmail.com>
2025-09-06 23:54:34 -0600

    Primer commit: proyecto base de React Native

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {  } from '@react-native/new-app-screen';
+import { SafeAreaView, StyleSheet, Text} from 'react-native';
+
+
+function App() {
+  
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hola Mundo</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+});
+
+export default App;
