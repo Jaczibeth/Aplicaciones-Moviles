ee5eee64a66ca9e53fbd7fc74f9e4fbbc7b94e62
Jaczibeth <jaczicruz@gmail.com>
2025-09-08 14:35:45 -0600

    Haciendo Uso de la etiqueta image

--- App.tsx
+++ App.tsx
@@ -6,15 +6,16 @@
  */
 
 import {  } from '@react-native/new-app-screen';
-import { SafeAreaView, StyleSheet, Text} from 'react-native';
-
+import { Image, SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App() {
   
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.holamundo} >Hola Mundo Jaczibeth</Text>
+      <Image style={{width:150,height:150, borderRadius:59}} source={{ uri: 'https://i.pinimg.com/originals/d5/ac/a0/d5aca0cd3681d4a0c0883644f19f1762.jpg ' }} /> 
+      <Text style={styles.h1} >Bienvenidos</Text>
+      <Text style={styles.h2} >Aplicaciones moviles</Text>
     </SafeAreaView>
     
   );
@@ -27,14 +28,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#77d0f6ff',
   },
-  holamundo: {
+  h1: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#ffffff',
+    color: '#0d0c0cff',
     fontFamily: 'georgia',
     borderRadius: 40,
     padding: 15,
-    backgroundColor: '#76a2d2ff',
+   // backgroundColor: '#76a2d2ff',
+  },
+  h2: {fontSize: 14, fontWeight: 'bold',color: '#0d0c0cff',fontFamily: 'georgia',borderRadius: 40, padding: 15,// backgroundColor: '#76a2d2ff',
   },
 });
 
