c2cbd43b1487370ea57db8a2a25bd02c3b34cf1b
Jaczibeth <jaczicruz@gmail.com>
2025-09-09 09:21:07 -0600

    Modificacion del card, era sin imagen

--- src/components/CardBasico.tsx
+++ src/components/CardBasico.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Image } from "react-native";
 export default function CardBasico() {
 
     return (
+        
         <View style={Styles.card}>
              <Text  style={Styles.Titulo}>Mi animal Favorito</Text>
              <Text  style={Styles.Contenido}> Los gatitos son animales tiernos, curiosos y 
@@ -12,8 +13,9 @@ export default function CardBasico() {
                 de su carácter cariñoso, son muy inteligentes y aprenden rápido a adaptarse a distintos
                  ambientes. Tener un gatito como compañero no solo brinda compañía, sino también amor
                   incondicional y momentos llenos de diversión. </Text>
-             <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} style={{ width: 100, height: 100, alignSelf:'center' }} />
         </View>
+        
+
 
     );
 }
