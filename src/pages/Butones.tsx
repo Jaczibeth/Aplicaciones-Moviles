13e634c277f573cb1593498d2cdc284a18a88e70
Jaczibeth <jaczicruz@gmail.com>
2025-09-09 00:13:39 -0600

    Agregando Icono al Boton

--- src/pages/Butones.tsx
+++ src/pages/Butones.tsx
@@ -1,5 +1,5 @@
 import {  Text, View, StyleSheet, Platform, StatusBar, TouchableOpacity} from 'react-native';
-
+import Icon  from 'react-native-vector-icons/FontAwesome';
 export default function Botones() {
   return (
     <View style={styles.mainS}>  
@@ -9,8 +9,9 @@ export default function Botones() {
             <Text style={styles.text}> Boton 1</Text>
         </TouchableOpacity>
          {/* Boton con icono*/}
-        <TouchableOpacity style={styles.botonbasico}>
-            <Text style={styles.text}> Boton 2</Text>
+        <TouchableOpacity style={styles.botonIcono}>
+            <Icon name="person"  size={20} style={styles.iconB}/>
+            <Text style={styles.text}> Boton Icono </Text>
         </TouchableOpacity>
 
     </View>
@@ -29,16 +30,31 @@ h1: {fontSize: 20,
     fontFamily: 'georgia',
 },
 botonbasico: {
-    backgroundColor: '#e2e7edff',
+    backgroundColor: '#c5e6f4ff',
     padding: 12,
     borderRadius: 10,
     marginTop: 10,
     marginBottom:10
 },
+
 text:{
     fontFamily:'georgia',
     color:'#0d0c0cff',
     fontWeight:'bold',
     textAlign:'center',
+    },
+    botonIcono: {
+    backgroundColor: '#c5e6f4ff',
+    fontSize: 30,
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+iconB: {
+    color: '#861889ff',
 }
 });
